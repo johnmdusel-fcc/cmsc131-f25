a73e1f50911151de4448f1ed65180ffb89b82533
John M Dusel <jdusel@frederick.edu>
2025-09-27 10:38:00 +0000

    FEEDBACK: Comments on Bank.java

--- src/projects/bank/Bank.java
+++ src/projects/bank/Bank.java
@@ -3,7 +3,7 @@ package projects.bank;
 public class Bank {
     private Account[] accounts;
     private int accountCount = 0;
-    public int index;
+    public int index;  // is this attribute necessary? consider removing
 
     // This bank will hold 392 accounts for this project.
     public Bank() {
@@ -26,23 +26,29 @@ public class Bank {
             return true;
         }
         // Else, simply add the account
+        // style: clearer to increment accountCount on separate line
         accounts[accountCount++] = account;
         return true;
     }
 
+    
+    // TODO convert line below into javadoc
     // returns the index of the account in the array or null if not found
     public int findAccountById(String ACCOUNTID) {
         for (int i = 0; i < accountCount; i++) {
             if (accounts[i].getAccountId().equals(ACCOUNTID)) {
-                index = i;
+                index = i;  // what's the intent of this line?
                 return i;
             }
         }
+        // TODO method does *not* return null. please correct javadoc.
         return -1; // Account not found
     }
 
+    // TODO convert line below to javadoc
     // New method to get the number of active accounts in the bank
     public int getNumberOfAccounts() {
+        // TODO use accountCount instead of doing this loop
         int count = 0;
         for (int i = 0; i < accounts.length; i++) {
             if (accounts[i] != null) {
@@ -52,13 +58,17 @@ public class Bank {
         return count;
     }
 
+    // This method wasn't included in the specs for phase 1
+    // TODO convert line below to javadoc
     // New method to get all accounts for a given account owner name
     public Account[] getAllAccountsByOwnerName(String accountOwnerName) {
         // Declare a temporary array to hold matches and set a match count to 100
+        // TODO why 10? consider using accounts.length
         Account[] accountsBySameOwner = new Account[10];
         int j = 0;
         for (int i = 0; i < accountCount; i++) {
             if (accounts[i].getAccountOwnerName().equals(accountOwnerName)) {
+                // style: it's clearer to increment j on a separate line
                 accountsBySameOwner[j++] = accounts[i];
             }
         }
