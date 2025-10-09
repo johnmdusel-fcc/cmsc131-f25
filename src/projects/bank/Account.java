9d8eb076ecb64bba25501c26633a8e367e863779
John M Dusel <jdusel@frederick.edu>
2025-10-09 10:14:20 +0000

    ENH: projects.bank: Account class

---
+++ src/projects/bank/Account.java
@@ -0,0 +1,51 @@
+package projects.bank;
+
+public class Account {
+
+    private final String id;
+    private final String ownerName;
+    private double balance;
+    private final AccountType type;
+
+    /**
+     * 
+     * @param id Unique alphanumeric id for this account.
+     * @param ownerName Name of this account's owner.
+     * @param balance Initial balance of this account.
+     * @param type Type of account this is.
+     * 
+     * @throws IllegalArgumentException If id is null or ownerName is null.
+     */
+    public Account(
+        String id,
+        String ownerName,
+        double balance,
+        AccountType type
+    ) {
+        if (id != null) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("id cannot be null");
+        }
+
+        if (ownerName != null) {
+            this.ownerName = ownerName;
+        } else {
+            throw new IllegalArgumentException("ownerName cannot be null");
+        }
+
+        if (type != null) {
+            this.type = type;
+        } else {
+            throw new IllegalArgumentException("type cannot be null");
+        }
+        
+        this.balance = balance;
+    }
+
+    public String getID() { return id; }
+    public String getOwnerName() { return ownerName; }
+    public double getBalance() {return balance; }
+    public AccountType getType() { return type; }
+
+}
