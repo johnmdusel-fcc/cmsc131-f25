5804c9547f63e5b62e24d05ca2182f1e6879b2d8
John M Dusel <jdusel@frederick.edu>
2025-10-12 11:15:14 +0000

    ENH: projects.bank: Account class phase 2
    - adds `make` static factory method
    
    - adds `toCSV` method, which aliases an override of `toString`

--- src/projects/bank/Account.java
+++ src/projects/bank/Account.java
@@ -48,4 +48,40 @@ public class Account {
     public double getBalance() {return balance; }
     public AccountType getType() { return type; }
 
+    /**
+     * Factory method for constructing an Account object from a CSV line.
+     * @param inputLine Eg, "savings,wz240833,Anna Gomez,8111.00"
+     * @return - new Account from supplied values.
+     * @throws {@code IllegalArgumentException}  if null {@code input} is null.
+     */
+    public static Account make(String inputLine) {
+        if (inputLine == null) {
+            throw new IllegalArgumentException("inputLine cannot be null");
+        }
+        String[] tokens = inputLine.split(",");
+        AccountType type = AccountType.valueOf(tokens[0].toUpperCase());
+        String id = tokens[1];
+        String ownerName = tokens[2];
+        double balance = (double) Double.valueOf(tokens[3]);
+        return new Account(id, ownerName, balance, type);
+    }
+
+    @Override 
+    public String toString() {
+        double balancedFormatted = (int) (getBalance() * 100) / 100.0;
+        return String.format(
+            "%s,%s,%s,%s",
+            getType().name().toLowerCase(),
+            getID(),
+            getOwnerName(),
+            balancedFormatted
+        );
+    }
+
+    /**
+     * CSV line holding this account's data.
+     * @return Eg, "savings,wz240833,Anna Gomez,8111.00"
+     */
+    public String toCSV() { return toString(); }
+
 }
