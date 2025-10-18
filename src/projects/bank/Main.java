75c584c47ce636d0e817416e040ca2fec6ba3612
John M Dusel <jdusel@frederick.edu>
2025-10-18 11:31:18 +0000

    ENH: projects.bank: Phase 2 driver code
    - Run after corrdcting loadAccounts and writeAccounts

--- src/projects/bank/Main.java
+++ src/projects/bank/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         phase1();
+        phase2();
     }
 
     public static void phase1() {
@@ -53,5 +54,17 @@ public class Main {
             e.printStackTrace();
         }
 
+    }
+
+    public static void phase2() {
+        String accountsFilename = "data/accounts.csv";
+        Bank bank = new Bank();
+        boolean result = bank.loadAccounts(accountsFilename);
+
+        System.out.println("Result of loading account: " + result);
+        System.out.println("Number of accounts: " + bank.getCount());
+
+        String outputFilename = "data/phase2.csv";
+        bank.writeAccounts(outputFilename);
     }
 }
